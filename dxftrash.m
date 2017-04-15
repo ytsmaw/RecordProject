42c6d928195f63e9a216b564bf0be3cced77296e
Willem Ytsma <wiy213>
2017-04-15 16:15:41 -0400

    I got it to work, but I did it by making millions of tiny lines because I just couldn't get LWPolyline to work. Now the file is huge and slow. I hate DXF R13. I hate it so much.

--- dxftrash.m
+++ dxftrash.m
@@ -6,7 +6,7 @@ x = x+diam/2;
 y=y+diam/2;
 
 %info: http://paulbourke.net/dataformats/dxf/min3d.html
-nel = 2; % number of elements
+nel = length(x); % number of elements
 
 if exist(strcat(pwd,'\Outputs\'),'dir') == false
     mkdir(strcat(pwd,'\Outputs\'));
@@ -14,26 +14,52 @@ if exist(strcat(pwd,'\Outputs\'),'dir') == false
 end
 dxfboy = fopen(strcat(pwd,'\Outputs\',fname),'w');
 
-% vv I think acadver AC1012 for R13 is what I want but it throws an error so I'm
-% just using AC1009 for R11-R12
-fprintf(dxfboy,'0\r\nSECTION\r\n2\r\nHEADER\r\n9\r\n$ACADVER\r\n1\r\nAC1009\r\n9\r\n$INSBASE\r\n10\r\n0.0\r\n20\r\n0.0\r\n30\r\n0.0\r\n9\r\n');
-fprintf(dxfboy,'$EXTMIN\r\n10\r\n0.0\r\n20\r\n0.0\r\n9\r\n$EXTMAX\r\n10\r\n%g\r\n20\r\n%g\r\n0\r\nENDSEC\r\n',diam,diam);
-fprintf(dxfboy,'0\r\nSECTION\r\n2\r\nTABLES\r\n0\r\nENDSEC\r\n0\r\nSECTION\r\n2\r\nBLOCKS\r\n0\r\nENDSEC\r\n');
-
-fprintf(dxfboy,'0\r\nSECTION\r\n2\r\nENTITIES\r\n');
 %{
-fprintf(dxfboy,'0\r\nLWPOLYLINE\r\n100\r\nAcDbPolyline\r\n5\r\nGROOVES\r\n8\r\n0\r\n90\r\n%d\r\n',nel);
-%fprintf(dxfboy,'39\r\n1\r\n'); % setting line thickness
+%% Head, Classes, Tables, Blocks.
+tp = fopen('Top.txt');
+tline = 'poop';
+while ischar(tline)
+    tline = fgets(tp);
+    if ischar(tline)
+        fprintf(dxfboy, '%s', tline);
+    end
+end
+fclose(tp);
+%}
+%% Entities Section
+fprintf(dxfboy,'0\r\nSECTION\r\n2\r\nENTITIES\r\n');
 
-for i = 1:nel %length(x)
-    fprintf(dxfboy,'10\r\n%g\r\n20\r\n%g\r\n',x(i),y(i));
+%fprintf(dxfboy,'0\r\nLWPOLYLINE\r\n100\r\nAcDbPolyline\r\n5\r\nGROOVES\r\n8\r\n0\r\n90\r\n%d\r\n',nel);
+%fprintf(dxfboy,'39\r\n1\r\n'); % setting line thickness
+tic
+for i = 2:10000
+    fprintf(dxfboy,'0\r\nLINE\r\n8\r\n0\r\n62\r\n1\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n11\r\n%g\r\n21\r\n%g\r\n31\r\n0\r\n',x(i-1),y(i-1),x(i),y(i));
+end
+tim = toc;
+fprintf('Estimated time: %3f minutes\n', (length(x)-10000)/10000 * tim /60);
+
+for i = 10001:nel %length(x)
+    fprintf(dxfboy,'0\r\nLINE\r\n8\r\n0\r\n62\r\n1\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n11\r\n%g\r\n21\r\n%g\r\n31\r\n0\r\n',x(i-1),y(i-1),x(i),y(i));
 end
 %}
-fprintf(dxfboy,'0\r\nLWPOLYLINE\r\n100\r\nAcDbPolyline\r\n100\r\nAcDbEntity\r\n\r\n330\r\n1F\r\n8\r\n0\r\n5\r\n1\r\n90\r\n2\r\n43\r\n0.0\r\n10\r\n0.0\r\n20\r\n0.0\r\n10\r\n%g\r\n20\r\n%g\r\n',diam,diam); % line across canvas
-fprintf(dxfboy,'0\r\nCIRCLE\r\n8\r\n1\r\n5\r\n2\r\n330\r\n1F\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,diam/2); % outer circle
-fprintf(dxfboy,'0\r\nCIRCLE\r\n8\r\n1\r\n5\r\n3\r\n330\r\n1F\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,.286/2); % inner hole
+%fprintf(dxfboy,'0\r\nLWPOLYLINE\r\n5\r\n1\r\n330\r\n1F\r\n100\r\nAcDbEntity\r\n8\r\n0\r\n100\r\nAcDbPolyline\r\n90\r\n2\r\n43\r\n0.0\r\n10\r\n0.0\r\n20\r\n0.0\r\n10\r\n%g\r\n20\r\n%g\r\n',diam,diam); % line across canvas
+fprintf(dxfboy,'0\r\nCIRCLE\r\n5\r\n2\r\n330\r\n1F\r\n100\r\nAcDbEntity\r\n8\r\n1\r\n100\r\nAcDbCircle\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,diam/2); % outer circle
+fprintf(dxfboy,'0\r\nCIRCLE\r\n5\r\n3\r\n330\r\n1F\r\n100\r\nAcDbEntity\r\n8\r\n1\r\n100\r\nAcDbCircle\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,.286/2); % inner hole
+fprintf(dxfboy,'0\r\nENDSEC\n');
 
-fprintf(dxfboy,'0\r\nENDSEC\r\n0\r\nEOF'); % end that file!
+%% Objects, end of file
+
+nd = fopen('end.txt');
+tline = 'poop';
+while ischar(tline)
+    tline = fgets(nd);
+    if ischar(tline)
+        fprintf(dxfboy, '%s', tline);
+    end
+end
+fclose(nd);
+
 fclose(dxfboy);
 msg='Done, losers!';
+end
     
