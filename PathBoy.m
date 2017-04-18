adf92dfa04eaaa8150cce97f81eb70a3f0aaeaa3
Willem Ytsma <wiy213>
2017-04-18 16:47:09 -0400

    Turns out I was calculating theta in an incorrect very complicated way

--- PathBoy.m
+++ PathBoy.m
@@ -4,24 +4,27 @@ close all
 
 
 %% inputs
-filename = 'BMSR-SunOrgan.mp3';
-GPI =40; %Grooves Per Inch (in)
-G_width = .004; %Max Groove width (in) equals number of rotations over space filled with grooves.
-rec_diam = 6; %Record diameter (in)
+filename = 'Warsh.mp3';
+GPI = 40; %Grooves Per Inch (in)
+G_width = .01; %Max Groove width (in) equals number of rotations over space filled with grooves.
+rec_diam = 8; %Record diameter (in)
 w = 45; %rotation speed (RPM)
 shrink_factor = 1; % 2 = 1/2 sampling rate, 3 = 1/3, etc.
-type = 'dxf'; %eps or svg
+type = 'dxf'; %eps or svg or dxf
 
 %% The calculation
 addpath('Inputs')
 [source, fs] = audioread(filename);
-
 source = RIAAprep(source,fs);
 source = source(shrink_factor:shrink_factor:length(source));
+
+entrancelength = .25 * GPI * 60/w; % sec lead in 
+source = [zeros(floor(entrancelength*fs),1); source]; % blank lead-in groove
+
 fs = fs/shrink_factor;
 len = length(source);
 
-outer_R = (rec_diam - .5)/2; % outer diameter of music
+outer_R = (rec_diam)/2; % outer diameter of music
 
 linPath = source'./(max(abs(source))) .* G_width/2; %linear path of music
 
@@ -29,22 +32,19 @@ numRev = (len/fs) / (60/w); % number of revolutions
 
 inner_R = outer_R - (GPI^-1 * numRev) % inner diameter of music
 
+pause()
+
 min_point_spacing = ( 1/(60/w)*(2*pi) ) / fs; %how far apart data points will be (in degrees) at the interior diameter.
 
 rot_num = (1:len) * (numRev/len); %a vector of full rotations
 
-space = min_point_spacing * ( outer_R ./ ( inner_R + (outer_R-inner_R) * (rot_num./numRev) ) ); %a calculation which determines the spacing of points radially to account for changing speed with diameter.
-
-disp('Commencing Theta summation');
-
-theta = zeros(1,len);
-theta(1) = 0;
-for i = 2:len
-    theta(i) = theta(i-1) + space(i); % sum the spaces between the points
-end
+theta = (1:len) * 2 * pi * (numRev/len); % This is actually theta, the first thing was needlessly complicated.
 
 spiral = inner_R + (1:len) * (outer_R-inner_R)/len; %creating the base spiral that the audio will be superposed upon
 rho = spiral + linPath; % the superposition!
+time = (1:len)/fs/60;
+figure()
+plot(time,rho);
 
 %% the display
 figure()
