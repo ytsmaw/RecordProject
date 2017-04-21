df7175f14cc697bcf147e6f113355aceebdd1519
Willem Ytsma <wiy213>
2017-04-20 20:25:04 -0400

    Leaned up the dxf a little

--- MATLAB & Laser Cut Record/PathBoy.m
+++ MATLAB & Laser Cut Record/PathBoy.m
@@ -5,18 +5,18 @@ close all
 
 %% inputs
 filename = 'Warsh.mp3';
-GPI = 40; %Grooves Per Inch (in)
+GPI = 30; %Grooves Per Inch (in)
 G_width = .01; %Max Groove width (in) equals number of rotations over space filled with grooves.
 rec_diam = 8; %Record diameter (in)
 w = 45; %rotation speed (RPM)
-shrink_factor = 1; % 2 = 1/2 sampling rate, 3 = 1/3, etc.
+shrink_factor = 1.5; % 2 = 1/2 sampling rate, 3 = 1/3, etc.
 type = 'dxf'; %eps or svg or dxf
 
 %% The calculation
 addpath('Inputs')
 [source, fs] = audioread(filename);
 source = RIAAprep(source,fs);
-source = source(shrink_factor:shrink_factor:length(source));
+source = source(floor(shrink_factor:shrink_factor:length(source)));
 
 entrancelength = .25 * GPI * 60/w; % sec lead in 
 source = [zeros(floor(entrancelength*fs),1); source]; % blank lead-in groove
@@ -40,7 +40,7 @@ rot_num = (1:len) * (numRev/len); %a vector of full rotations
 
 theta = (1:len) * 2 * pi * (numRev/len); % This is actually theta, the first thing was needlessly complicated.
 
-spiral = inner_R + (1:len) * (outer_R-inner_R)/len; %creating the base spiral that the audio will be superposed upon
+spiral = outer_R - (1:len) * (outer_R-inner_R)/len; %creating the base spiral that the audio will be superposed upon
 rho = spiral + linPath; % the superposition!
 time = (1:len)/fs/60;
 figure()
@@ -72,7 +72,7 @@ switch type
     m = svgtrash(x,y,rec_diam, rec_diam, fname); %converting X-Y coordinates to SVG
     disp(m);
     case 'dxf'
-    fname = sprintf('%s_d%ggpi%gw%g.dxf', filename(1:(length(filename)-4)),rec_diam,GPI,w); %creating the file name
+    fname = sprintf('%s_d%ggpi%gw%gs%g.dxf', filename(1:(length(filename)-4)),rec_diam,GPI,w,shrink_factor); %creating the file name
     disp('Commencing DXF creation')
     m = dxftrash(x,y,rec_diam, fname); %converting X-Y coordinates to SVG
     disp(m);
--- MATLAB & Laser Cut Record/dxftrash.m
+++ MATLAB & Laser Cut Record/dxftrash.m
@@ -14,8 +14,8 @@ if exist(strcat(pwd,'\Outputs\'),'dir') == false
 end
 dxfboy = fopen(strcat(pwd,'\Outputs\',fname),'w');
 
-%{
-%% Head, Classes, Tables, Blocks.
+
+%% Head, Tables
 tp = fopen('Top.txt');
 tline = 'poop';
 while ischar(tline)
@@ -25,7 +25,7 @@ while ischar(tline)
     end
 end
 fclose(tp);
-%}
+
 %% Entities Section
 fprintf(dxfboy,'0\r\nSECTION\r\n2\r\nENTITIES\r\n');
 
@@ -33,18 +33,18 @@ fprintf(dxfboy,'0\r\nSECTION\r\n2\r\nENTITIES\r\n');
 %fprintf(dxfboy,'39\r\n1\r\n'); % setting line thickness
 tic
 for i = 2:10000
-    fprintf(dxfboy,'0\r\nLINE\r\n8\r\n0\r\n62\r\n1\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n11\r\n%g\r\n21\r\n%g\r\n31\r\n0\r\n',x(i-1),y(i-1),x(i),y(i));
+    fprintf(dxfboy,'0\r\nLINE\r\n8\r\n0\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n11\r\n%g\r\n21\r\n%g\r\n31\r\n0\r\n',x(i-1),y(i-1),x(i),y(i));
 end
 tim = toc;
 fprintf('Estimated time: %3f minutes\n', (length(x)-10000)/10000 * tim /60);
 
 for i = 10001:nel %length(x)
-    fprintf(dxfboy,'0\r\nLINE\r\n8\r\n0\r\n62\r\n1\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n11\r\n%g\r\n21\r\n%g\r\n31\r\n0\r\n',x(i-1),y(i-1),x(i),y(i));
+    fprintf(dxfboy,'0\r\nLINE\r\n8\r\n0\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n11\r\n%g\r\n21\r\n%g\r\n31\r\n0\r\n',x(i-1),y(i-1),x(i),y(i));
 end
 %}
 %fprintf(dxfboy,'0\r\nLWPOLYLINE\r\n5\r\n1\r\n330\r\n1F\r\n100\r\nAcDbEntity\r\n8\r\n0\r\n100\r\nAcDbPolyline\r\n90\r\n2\r\n43\r\n0.0\r\n10\r\n0.0\r\n20\r\n0.0\r\n10\r\n%g\r\n20\r\n%g\r\n',diam,diam); % line across canvas
-fprintf(dxfboy,'0\r\nCIRCLE\r\n5\r\n2\r\n330\r\n1F\r\n100\r\nAcDbEntity\r\n8\r\n1\r\n100\r\nAcDbCircle\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,diam/2); % outer circle
-fprintf(dxfboy,'0\r\nCIRCLE\r\n5\r\n3\r\n330\r\n1F\r\n100\r\nAcDbEntity\r\n8\r\n1\r\n100\r\nAcDbCircle\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,.286/2); % inner hole
+fprintf(dxfboy,'0\r\nCIRCLE\r\n8\r\n1\r\n100\r\nAcDbCircle\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,diam/2); % outer circle
+fprintf(dxfboy,'0\r\nCIRCLE\r\n8\r\n1\r\n100\r\nAcDbCircle\r\n10\r\n%g\r\n20\r\n%g\r\n30\r\n0\r\n40\r\n%g\r\n',diam/2,diam/2,.286/2); % inner hole
 fprintf(dxfboy,'0\r\nENDSEC\n');
 
 %% Objects, end of file
